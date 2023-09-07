bd32a985e3e4039c1a1501f2c78b3f7266e05073
Mehul Patel <mehulpatel510@gmail.com>
2023-09-07 17:14:55 +0530

    First Commit for Smart Task

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import TaskApp from './TaskApp';
+
+function App() {
+  return (
+  <div className="App">
+    <TaskApp />
+    
+  </div>
+);
+}
+
+export default App
---
+++ smarter-tasks/src/TaskList.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Task from "./Task";
+import { TaskItem } from "./types";
+
+interface Props {
+    tasks: TaskItem[];
+}
+interface State {
+//   tasks: TaskItem[];
+}
+
+class TaskList extends React.Component<Props, State> {
+    // constructor(props: Props){
+    //     super(props);
+    //     this.state = {
+    //         tasks: [{ title: "Pay rent" }, { title: "Submit assignment" }],
+    //     };
+    // }
+    // componentDidMount() {
+    //     const   tasks = [{ title: "Pay rent" }, { title: "Submit assignment...." }]
+    //     this.setState((state, props) => ({
+    //       tasks,
+    //       }))
+        
+    // }
+  render(){
+    return (
+        this.props.tasks.map((task, idx) => <Task key={idx} title={task.title} />)
+        );
+  }
+}
+
+export default TaskList;
