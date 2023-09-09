b57106beb47d0a79118d060b868f809367011ca9
Mehul Patel <mehulpatel510@gmail.com>
2023-09-09 15:55:59 +0530

    update notfound route

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -3,8 +3,8 @@ export default function PageNotFound() {
 
   return (
     <div>
-      <h1>404 Page Not Found!</h1>
-      <form action="/home">
+      <h1 className="p-4">404</h1>
+      <form action="/">
         <button id="backToHomeButton"
           type="submit"
           className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray"
