6f1916e5b4fd71679733214a91fb519e79a9e004
Mehul Patel <mehulpatel510@gmail.com>
2023-09-11 10:48:17 +0530

    Update notfound page

--- smarter-tasks/src/Notfound.tsx
+++ smarter-tasks/src/Notfound.tsx
@@ -1,17 +1,16 @@
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
 
   return (
     <div>
       <h1 className="p-4">404</h1>
-      <form action="/">
+      <Link to="/">
         <button id="backToHomeButton"
           type="submit"
           className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray"
-        >
-          Back to Home
-        </button>
-      </form>
+         >Home</button>
+      </Link>
     </div>
   );
 }
